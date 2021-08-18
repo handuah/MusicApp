9c086ea202fc07c6330d6a54a640015f86827285
handuah <handuah@gmail.com>
2021-08-18 13:00:26 +0000

    created welcome screen

---
+++ lib/screens/welcomeScreen.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_app/styles/colors.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  // const WelcomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+        backgroundColor: AppColors.darkerblue,
+        body: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: screenHeight * 0.55,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2020%2F06%2F09%2FBest-Albums-of-the-year3.jpg&q=85',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: screenHeight * 0.6,
+                        width: screenWidth,
+                        color: AppColors.black.withOpacity(0.5),
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Stack(
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.play,
+                                  color: AppColors.white,
+                                  size: 80.0,
+                                ),
+                                Positioned(
+                                  top: screenHeight * 0.035,
+                                  left: screenWidth * 0.05,
+                                  child: FaIcon(
+                                    FontAwesomeIcons.play,
+                                    color: AppColors.darkblue.withOpacity(0.8),
+                                    size: 55.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: screenHeight * 0.02,
+                            ),
+                            Text(
+                              'MusicApp',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.montserrat(
+                                color: AppColors.white,
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Padding(
+                //   padding: EdgeInsets.symmetric(
+                //     horizontal: screenWidth * 0.06,
+                //   ),
+                //   child: ,
+                // ),
+                SizedBox(
+                  height: screenHeight * 0.02,
+                ),
+                Center(
+                  child: Container(
+                    width: screenWidth * 0.6,
+                    height: screenHeight * 0.08,
+                    // color: Colors.red,
+                    child: Text(
+                      'Enjoy the best music app!',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Center(
+                  child: Container(
+                    width: screenWidth * 0.7,
+                    height: screenHeight * 0.09,
+                    // color: Colors.red,
+                    child: Text(
+                      'This App will provide you with the best  music experience.',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.montserrat(
+                        color: AppColors.white.withOpacity(0.8),
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Center(
+                  child: SizedBox(
+                    height: screenHeight * 0.07,
+                    width: screenWidth * 0.8,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.montserrat(
+                          color: AppColors.darkblue,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: AppColors.lighterblue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Center(
+                  child: SizedBox(
+                    height: screenHeight * 0.07,
+                    width: screenWidth * 0.8,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Create New Account',
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.montserrat(
+                          color: AppColors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: AppColors.darkerblue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ));
+  }
+}
