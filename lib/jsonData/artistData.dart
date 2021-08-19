6c039f02681294a30972621e56ad9c7db96a095b
handuah <handuah@gmail.com>
2021-08-19 13:35:27 +0000

    initial commit

---
+++ lib/jsonData/artistData.dart
@@ -0,0 +1,46 @@
+class ArtistData {
+  static final getData = [
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+  ];
+}
