83a98fe252476785fdc9983826ff625e9861be5e
handuah <handuah@gmail.com>
2021-08-19 13:35:50 +0000

    completed first card on trending music

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -88,18 +88,64 @@ class _HomePageState extends State<HomePage> {
             height: screenHeight * 0.2,
             width: screenWidth,
             decoration: BoxDecoration(
-              color: Colors.red,
-            ),
+                // color: Colors.red,
+                ),
             child: ListView(
               scrollDirection: Axis.horizontal,
               children: [
-                Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
+                Stack(
+                  children: [
+                    Container(
+                      height: screenHeight * 0.2,
+                      width: screenWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: screenHeight * 0.2,
+                      width: screenWidth * 0.9,
+                      decoration: BoxDecoration(
+                        color: AppColors.darkblue.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    Positioned(
+                      right: screenWidth * 0.04,
+                      bottom: screenHeight * 0.02,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'The Weekend',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.montserrat(
+                              color: AppColors.white,
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Blinding Lights',
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.montserrat(
+                              color: AppColors.white,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   width: screenWidth * 0.04,
