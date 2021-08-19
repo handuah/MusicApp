48fc0c3410dcac25fd98e5afd27c6ca8581f2c08
handuah <handuah@gmail.com>
2021-08-19 14:24:17 +0000

    created listview calling json data from Trending Music

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:hexcolor/hexcolor.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart'
+// import 'package:hexcolor/hexcolor.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:music_app/styles/colors.dart';
+import 'package:music_app/jsonData/trendingMusic.dart';
 
 class HomePage extends StatefulWidget {
   // const HomePage({ Key? key }) : super(key: key);
@@ -13,6 +13,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // initializzing artist data
+  var artisticDetails = TrendingMusic.getData;
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -90,90 +93,83 @@ class _HomePageState extends State<HomePage> {
             decoration: BoxDecoration(
                 // color: Colors.red,
                 ),
-            child: ListView(
+            child: ListView.separated(
+              // padding: EdgeInsets.only(
+              //     bottom: screenHeight * 0.02, top: screenHeight * 0.01),
+              separatorBuilder: (BuildContext context, int index) => SizedBox(
+                width: screenWidth * 0.03,
+              ), //add a separater to the list
               scrollDirection: Axis.horizontal,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      height: screenHeight * 0.2,
-                      width: screenWidth * 0.9,
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(10.0),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                            'https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg',
-                          ),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      height: screenHeight * 0.2,
-                      width: screenWidth * 0.9,
-                      decoration: BoxDecoration(
-                        color: AppColors.darkblue.withOpacity(0.4),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    Positioned(
-                      right: screenWidth * 0.04,
-                      bottom: screenHeight * 0.02,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'The Weekend',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.montserrat(
-                              color: AppColors.white,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'Blinding Lights',
-                            textAlign: TextAlign.center,
-                            style: GoogleFonts.montserrat(
-                              color: AppColors.white,
-                              fontSize: 12.0,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: screenWidth * 0.04,
-                ),
-                Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-                SizedBox(
-                  width: screenWidth * 0.04,
-                ),
-                Container(
-                  height: screenHeight * 0.2,
-                  width: screenWidth * 0.8,
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),
-              ],
+              itemCount: artisticDetails.length,
+              itemBuilder: (BuildContext trendingCtxt, int trendMusInt) =>
+                  _trendingMusicCard(trendingCtxt, trendMusInt),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  // TRENDING MUSIC CARD
+  Widget _trendingMusicCard(BuildContext trendingCtxt, int trendMusInt) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      children: [
+        Container(
+          height: screenHeight * 0.2,
+          width: screenWidth * 0.9,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                artisticDetails[trendMusInt]['albumImgPath'],
+                // 'https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          height: screenHeight * 0.2,
+          width: screenWidth * 0.9,
+          decoration: BoxDecoration(
+            color: AppColors.darkblue.withOpacity(0.4),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        Positioned(
+          right: screenWidth * 0.04,
+          bottom: screenHeight * 0.02,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                artisticDetails[trendMusInt]['artistName'],
+                // 'The Weekend',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  color: AppColors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                artisticDetails[trendMusInt]['albumName'],
+                // 'Blinding Lights',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  color: AppColors.white,
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
