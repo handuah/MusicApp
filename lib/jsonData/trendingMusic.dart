7249f8095282896d7c57afc9a356f5c3813e1264
handuah <handuah@gmail.com>
2021-08-19 14:23:53 +0000

    renamed to trendingn Music from ArtisyData

---
+++ lib/jsonData/trendingMusic.dart
@@ -0,0 +1,45 @@
+class TrendingMusic {
+  static final getData = [
+    {
+      "albumImgPath":
+          "https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg",
+      "artistName": "The Weekend",
+      "albumName": "Blinding Lights",
+      "songName": "Save Your Tears",
+    },
+    {
+      "albumImgPath":
+          "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/wujpwni84kwzxje8sooi/billie-eilish-press-photo-2021?fimg-ssr-default",
+      "artistName": "Billie Eilish",
+      "albumName": "Happier Than Ever",
+      "songName": "Lost Cause",
+    },
+    {
+      "albumImgPath": "https://i1.wp.com/i.imgur.com/pnfIdJ6.jpg",
+      "artistName": "Shreya Ghoshal",
+      "albumName": "Hum Adhuri Kahani",
+      "songName": "Hasi Ban Gaye",
+    },
+    {
+      "albumImgPath":
+          "https://ibighit.com/bts/images/bts/discography/butter-2/butter-2-cover.jpg",
+      "artistName": "BTS",
+      "albumName": "Butter",
+      "songName": "Butter",
+    },
+    {
+      "albumImgPath":
+          "https://i.ytimg.com/vi/orJSJGHjBLI/maxresdefault.jpg?v=60d557c9",
+      "artistName": "Ed Sheeran",
+      "albumName": "Bad Habits",
+      "songName": "Bad Habits",
+    },
+    {
+      "albumImgPath":
+          "https://images.complex.com/complex/images/c_fill,dpr_auto,f_auto,q_auto,w_1400/fl_lossy,pg_1/o9cn3eq2u1gtnx6vsbyv/the-kid-laroi-fck-love-3-over-you?fimg-ssr-default",
+      "artistName": "The Kid Laroi",
+      "albumName": "Fuck Love 3+",
+      "songName": "Stay",
+    },
+  ];
+}
