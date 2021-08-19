c8d3ec5520eb6f96cfb8af4a6a097d132e19dd92
handuah <handuah@gmail.com>
2021-08-19 11:39:07 +0000

    created homePage with name and avatar

---
+++ lib/screens/homePage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:music_app/styles/colors.dart';
+
+class HomePage extends StatefulWidget {
+  // const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: AppColors.darkerblue,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: screenWidth * 0.04,
+        ),
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              top: screenHeight * 0.08,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Hello Hannah',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    color: AppColors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: AppColors.lightblue,
+                  maxRadius: 30.0,
+                  child: CircleAvatar(
+                    backgroundColor: AppColors.darkblue,
+                    maxRadius: 28.0,
+                    backgroundImage: NetworkImage(
+                        'https://media-exp1.licdn.com/dms/image/C4D03AQHBc2B31Q5YBA/profile-displayphoto-shrink_400_400/0/1592216299456?e=1634774400&v=beta&t=SLpzcedjDr5KFAWiO6VKptTNmw0FjuicRTXqLmJh8EY'),
+                    // child: Image(
+                    //   image: NetworkImage(
+                    //     'https://media-exp1.licdn.com/dms/image/C4D03AQHBc2B31Q5YBA/profile-displayphoto-shrink_400_400/0/1592216299456?e=1634774400&v=beta&t=SLpzcedjDr5KFAWiO6VKptTNmw0FjuicRTXqLmJh8EY',
+                    //   ),
+                    // ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
