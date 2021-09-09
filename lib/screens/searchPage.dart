12363f4f95cc34d6b2249db9f386bf9e2c530586
handuah <handuah@gmail.com>
2021-09-09 12:17:41 +0000

    created first search result style

--- lib/screens/searchPage.dart
+++ lib/screens/searchPage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart'
 // import 'package:hexcolor/hexcolor.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:music_app/styles/colors.dart';
 import 'package:music_app/jsonData/trendingMusic.dart';
 
@@ -78,6 +79,84 @@ class _SearchPageState extends State<SearchPage> {
               color: AppColors.white,
               fontSize: 18.0,
               fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: screenHeight * 0.03,
+          ),
+          Container(
+            width: screenWidth,
+            height: screenHeight * 0.16,
+            decoration: BoxDecoration(
+              color: HexColor("#181841"),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.01,
+                vertical: screenHeight * 0.01,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Material(
+                    elevation: 4.0,
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Container(
+                      width: screenWidth * 0.28,
+                      height: screenHeight * 0.16,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.circular(20.0),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                              'https://www.pluggedin.com/wp-content/uploads/2021/01/Weeknd-Blinding-Lights-WP.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: screenWidth * 0.03,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Billie Eilish',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.montserrat(
+                          color: AppColors.white,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      Text(
+                        'Your Power',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.montserrat(
+                          color: AppColors.white,
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: screenWidth * 0.34,
+                  ),
+                  Text(
+                    '3:57',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.montserrat(
+                      color: AppColors.white,
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
